3b37dfd40e3432651eab85f74702a8d6986e0e2f
等一个人咖啡 <1573588748@qq.com>
2024-12-28 11:02:33 +0800

    单元测试

--- test/fundme.test.js
+++ test/fundme.test.js
@@ -1,4 +1,7 @@
 const {ethers} = require("hardhat")
+const {assert, expect} = require("chai")
+//用来模拟时间流失
+const helpers = require("@nomicfoundation/hardhat-network-helpers")
 
 
 describe("test fundme contract", async function() {
@@ -25,6 +28,7 @@ describe("test fundme contract", async function() {
         
     })
 
+
     it("test if the owner is msg.sender", async function() {
         // const [firstAccount] = await ethers.getSigners()
         // const fundMeFactory = await ethers.getContractFactory("FundMe")
@@ -33,4 +37,111 @@ describe("test fundme contract", async function() {
         await fundMe.waitForDeployment()
         assert.equal((await fundMe.owner()), firstAccount)
     })
+
+    it("window close, value grater thean minium, fund failed",
+        async function () {
+            //lockTime传入的是180，所以这里时间流逝 200秒
+            //这个命令用于增加模拟的以太坊时间 增加时间200秒
+            await helpers.time.increase(200)
+            //模拟挖矿--挖掘新区块，使得增加时间的交易被确认
+            await helpers.mine()
+            //to.be.revertedWith 是一个 Chai 断言库中的异步断言，用于检查交易是否被撤销，并提供了撤销的原因
+            await expect(fundMe.fund({value: ethers.parseEther("0.1")})).to.be.revertedWith("window is closed")
+        }
+    )
+
+    it("window open, value less thean minium, fund failed",
+        async function () {
+            await expect(fundMe.fund({value: ethers.parseEther("0.01")})).to.be.revertedWith("Send more ETH")
+        }
+    )
+
+    it("window close, value is grater minimum, fund success",
+        async function () {
+            await fundMe.fund({value: ethers.parseEther("0.1")})
+            const balance = await fundMe.fundersToAmount(firstAccount)
+            await expect(balance).to.equal(ethers.parseEther("0.1"))
+        }
+    )
+
+    it("not owner, window close, target reache, retFund failed",
+        async function () {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            await helpers.time.increase(200)
+            await helpers.mine()
+            await expect(secondFundMe.getFund()).to.be.revertedWith("this function can only be called by owner")
+        }
+    )
+
+    it("owner, window open, target reache, retFund failed",
+        async function () {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            await expect(fundMe.getFund()).to.be.revertedWith("window is not closed")
+        }
+    )
+
+    it("window close, target not reached, getFund failed",
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("0.1")})
+            //超过180 make sure the window is closed
+            await helpers.time.increase(200)
+            //模拟挖矿，使得时间超过200秒
+            await helpers.mine()
+            await expect(fundMe.getFund()).to.be.revertedWith("Target is not reached")
+        }
+    )
+
+    it("window close, target reached, getFund success",
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            //超过180 make sure the window is closed
+            await helpers.time.increase(200)
+            //模拟挖矿，使得时间超过200秒
+            await helpers.mine()
+            //执行成功会出发一个 Event事件 withArgs：事件参数
+            await expect(fundMe.getFund()).to
+            .emit(fundMe, "FundWithdDrawByOwner")
+            .withArgs(ethers.parseEther("1"))
+        }
+    )
+
+    //reFund
+    //window close,target not reache, funder has balance
+    it("window open, target not reached, funder has balance", 
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("0.1")})
+            expect(fundMe.refund()).to.be.revertedWith("window is not closed")
+        }
+    )
+
+    it("window closed, target reached, funder has balance", 
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            await helpers.time.increase(200)
+            await helpers.mine()
+            expect(fundMe.refund()).to.be.revertedWith("Target is reached")
+        }
+    )
+
+    it("window closed, target reached, funder no balance", 
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            await helpers.time.increase(200)
+            await helpers.mine()
+            expect(secondFundMe.refund()).to.be.revertedWith("there is no fund for you")
+        }
+    )
+
+    it("window closed, target reached, funder has balance", 
+        async function() {
+            await fundMe.fund({value: ethers.parseEther("1")})
+            await helpers.time.increase(200)
+            await helpers.mine()
+            expect(fundMe.refund()).to.emit(fundMe, "RefundBalance")
+            .withArgs(firstAccount, ethers.parseEther("1"))
+            // const balance = await fundMe.fundersToAmount(firstAccount)
+            // expect(balance).to.equals(ethers.parseEther("1"))
+        }
+    )
+
 })
